7bfe3e0bb9a73ce53bcbb5f8edc28d8b80d9294f
AARON JOHNSON <jardaaroncs@gmail.com>
2024-04-27 17:12:03 -0600

    feat add and connect spotify app boilerplate

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("baseapp.urls")),
     path('admin/', admin.site.urls),
+    path("", include("baseapp.urls")),
+    path("spotify/", include("spotify.urls"))
 ]
